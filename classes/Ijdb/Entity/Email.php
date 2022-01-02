1ac1e647081eb85f928b937d5a2237131ebcc52b
Andrei Chornii <andrei.chornii@gmail.com>
2022-01-02 18:23:48 +0200

    double_emails

--- classes/Ijdb/Entity/Email.php
+++ classes/Ijdb/Entity/Email.php
@@ -7,7 +7,7 @@ namespace Ijdb\Entity;
 class Email
 {
 
-//    const EMAILS_TO_SEND = ['andrei.chornii@gmail.com', 'avtoadvocat-dp@ukr.net'];
-    const EMAILS_TO_SEND = ['andrei.chornii@gmail.com'];
+    const EMAILS_TO_SEND = ['andrei.chornii@gmail.com', 'avtoadvocat-dp@ukr.net'];
+//    const EMAILS_TO_SEND = ['andrei.chornii@gmail.com'];
 
 }
